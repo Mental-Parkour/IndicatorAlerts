809c0a16afaa1370ebfce24ea0475d7e4ec56a4d
Mental-Parkour <44530689+Mental-Parkour@users.noreply.github.com>
2021-09-18 19:37:00 +0400

    Delete Reciever.ts

--- Reciever.ts
+++
@@ -1,9 +0,0 @@
-import {WebSocket} from 'ws';
-
-const HOST = 'ws://localhost:3000';
-console.log(HOST);
-const ws = new WebSocket(HOST);
-
-ws.onmessage = function (event) {
-	console.log( event.data );
-};
